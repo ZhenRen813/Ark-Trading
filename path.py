696805be9fcd89da2f8570800458d8f481e2a0f7
Aaron <kyrios.ark@gmail.com>
2025-04-21 19:41:30 +0800

    add script

---
+++ path.py
@@ -0,0 +1,3 @@
+import sys
+
+sys.path.append('/root/dev/Ark-Trading/waterstart')
--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source /root/venv/bin/activate
+python path.py
 nohup /root/venv/bin/python /root/dev/trading2/tests/livetrendbar_test.py > output.log 2>&1
